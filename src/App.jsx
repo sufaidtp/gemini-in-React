0e9cc960ec5fa4c3a32186dac7786ef9e7039efb
sufaid tp <sufaidtp@sufaids-MacBook-Air.local>
2025-07-01 21:57:35 +0530

    Fix: Rename sidebar to Sidebar for case-sensitive build

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Sidebar from './components/sidebar/sidebar.jsx'
+import Sidebar from './components/Sidebar/Sidebar.jsx'
 import Main from './components/Main/Main.jsx'
 
 const App = () => {
